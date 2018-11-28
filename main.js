b2fa506b029432da535bfd0cdcebd7c2ab2206e6
DiegoA <diegoaneli@gmail.com>
2018-11-28 17:12:05 +0100

    parte quadrati 1

---
+++ main.js
@@ -0,0 +1,6 @@
+//creare una griglia formata da 8x8 quadratini tutti bianchi
+//15 di questi quadratini (scelti a caso dall inizio)
+//SE cliccati diventano ROSSI
+//E gli altri diventano verdi
+//PLUS : Sopra alla griglia c è un contatore
+//       che conta quanti rossi e verdi sono stati scoperti
