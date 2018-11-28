e4f7939f2f5b672f37fba3f97fcef6b694586f27
DiegoA <diegoaneli@gmail.com>
2018-11-28 21:50:06 +0100

    jquery-quadratini

--- main.js
+++ main.js
@@ -4,3 +4,36 @@
 //E gli altri diventano verdi
 //PLUS : Sopra alla griglia c è un contatore
 //       che conta quanti rossi e verdi sono stati scoperti
+
+
+alert("Scegli 15 caselle");
+
+// .elemento.hasClass('active')
+// console.log(griglia.hasClass('active'));
+// true;false
+// $('.griglia .square')hasClass(active)) {
+
+var griglia = $('#pippo')
+
+//se cliccati diventano rossi
+$('.griglia .square').click(function(){
+  if ($(this).hasClass('pippo')) {
+    $(this).addClass('red').show(6000)
+    alert('Hai perso!')
+  }
+  else { //altrimenti verdi (GLI ALTRI)
+    $(this).addClass('green')
+  }
+});
+
+
+// 
+// $(document).ready(function() {
+// var count = 0;
+//
+//   $("#update").click(function() {
+//     count++;
+//     $("#counter").html("I quadratini premuti verdi sono : "+count);
+//   }
+//
+// });
